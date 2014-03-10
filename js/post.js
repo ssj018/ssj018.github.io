ec20fa430d687526d0cc3c74f564f7aab2d264f5
Lingxian Kong <konglingxian@huawei.com>
2014-03-10 14:38:34 +0800

    test for the entry

--- js/post.js
+++ js/post.js
@@ -130,7 +130,7 @@ $(document).ready(function(){
             };
         })();
 
-        if($('.entry h2').length > 2 && !isMobile.any() && !ie6){
+        if($('.entry h2').length > 1 && !isMobile.any() && !ie6){
 
             genIndex();
 
